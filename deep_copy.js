af2a8c6280d21aaf96368c0e81599c2cb3d44046
kodmi <chubakabra@yandex.ru>
2020-10-14 10:33:25 +0400

    add function body and tests

--- deep_copy.js
+++ deep_copy.js
@@ -1,4 +1,9 @@
  const deepCopy = (obj) => {
-    return obj
+    const copied = {};
+    for (let key in obj) {
+        if (typeof(obj[key]) !== "object") copied[key] = obj[key];
+        else copied[key] = deepCopy(obj[key])
+    }
+    return copied;
  };
  export default deepCopy;
--- deep_copy.test.js
+++ deep_copy.test.js
@@ -1,5 +1,46 @@
 import deepCopy from './deep_copy';
+const original = {
+    primitive: 5,
+    obj: {primitive: "test", obj: {}},
+    arr: [{primitive: "prim1", obj: {a: "a"}},{primitive: "prim2", obj: {b:"b"}}],
+    method: () => "test method",
+}
 
-test('two plus two is four', () => {
-    expect(2 + 2).toBe(4);
-  });
+test('these are different objects', () => {
+    const copied = deepCopy(original);
+    copied.primitive = 6;
+    expect(copied.primitive).not.toBe(original.primitive);
+});
+
+test('object primitives are equal ', () => {
+    const copied = deepCopy(original);
+    expect(copied.primitive).toBe(original.primitive);
+});
+
+test('inner objects are different', () => {
+    const copied = deepCopy(original);
+    copied.obj.primitive = "test inner object";
+    expect(copied.obj.primitive).not.toBe(original.obj.primitive);
+});
+
+test('method was copied', () => {
+    const copied = deepCopy(original);    
+    expect(copied.method()).toBe("test method");
+});
+
+test('objects methods are different', () => {
+    const copied = deepCopy(original); 
+    copied.method = () => "other test method";   
+    expect(copied.method()).not.toBe(original.method());
+});
+
+test('array was copied', () => {
+    const copied = deepCopy(original);   
+    expect(copied.arr.lenght).toBe(original.arr.lenght);
+});
+
+test('objects arrays are different', () => {
+    const copied = deepCopy(original);
+    copied.arr[0].obj.a = "c"   
+    expect(copied.arr[0].obj.a).not.toBe(original.arr[0].obj.a);
+});
